a778a9a36c522498b773a1b17773c5e313b5ab21
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-23 16:37:00 -0700

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+features:
+
+- [ ] check that package-lock.json matches package.json
+- [ ] ensure package-lock.json reflects node_modules
+- [ ] check for require() statements
+- [ ] ts support
+- [ ] warn if node version incorrect
+- [ ] node versioning based on .node-version or engines
+- [ ] edon add (npm install foo)
+- [ ] edon install (npm install -g foo)
+- [ ] edon update (notify if transitives out-of-date)
+- [ ] edon outdated
+- [ ] edon link
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::process::{Command, Stdio};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let cmd = &args[1];
+
+    match cmd.as_ref() {
+        "check" => check(),
+        "run" => run(),
+        _ => panic!("invalid command: {}", cmd),
+    }
+}
+
+fn check() {
+    println!("check");
+}
+
+fn run() {
+    println!("running");
+    let proc = Command::new("node")
+        .arg("-v")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Failed to start echo process");
+    proc.wait_with_output().unwrap();
 }
